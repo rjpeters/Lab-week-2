a8ebc9f0f712dadfbf46be329f463187b9b1307c
rjpeters <rikpeters.rjp@gmail.com>
2022-09-14 16:22:59 +0200

    Make some edits to lab 3

--- sqllab3.sql
+++ sqllab3.sql
@@ -1,37 +1,17 @@
 USE sakila;
 
+
 --    In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
-
--- This one was tricky for me. First I tried a somewhat convoluted method that I googled together (with a 'Self Join')
-SELECT last_name FROM sakila.actor a
-WHERE NOT EXISTS (select 1 
-         from sakila.actor b
-         where b.last_name = a.last_name
-         and b.first_name <> a.first_name);
-
--- Then there's another, easier way, where the output is less clean (because you also get the stuff you don't want, albeit at the bottom of the list):
 SELECT last_name, COUNT(first_name) as how_many FROM sakila.actor
 GROUP BY last_name
-ORDER BY how_many ASC;
--- And then copy-paste the names that have '1' in the how_many column - there's your answer
-
+HAVING how_many = 1;
 
 
 --    Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
--- I'm pretty sure it is possible to do this more simply than what I'm doing here, but can't figure out how right now.
-
--- Again, I have two different methods. The convoluted join method...
-SELECT last_name FROM sakila.actor a
-WHERE EXISTS (select 1 
-         from sakila.actor b
-         where b.last_name = a.last_name
-         and b.first_name <> a.first_name);
-
--- And the dirty 'group by' method (but here with DESC, to give last names with higher frequencies than 1):
 SELECT last_name, COUNT(first_name) as how_many FROM sakila.actor
 GROUP BY last_name
-ORDER BY how_many DESC;
-
+HAVING how_many > 1
+ORDER BY how_many ASC;
 
 
 --    Using the rental table, find out how many rentals were processed by each employee.
