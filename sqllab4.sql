970f0b9c3365563dacb09f864400a616542bc0a5
rjpeters <rikpeters.rjp@gmail.com>
2022-09-15 10:13:06 +0200

    Update lab4, add lab5 file

--- sqllab4.sql
+++ sqllab4.sql
@@ -1,45 +1,52 @@
 USE sakila;
+
+
    -- How many films are there for each of the categories in the category table. Use appropriate join to write this query.
 SELECT category.name, count(film_category.film_id) FROM category
-INNER JOIN film_category ON category.category_id=film_category.category_id
+JOIN film_category ON category.category_id=film_category.category_id
 GROUP BY category.name;
 
-   
+
+SELECT payment_date FROM payment;
+
    -- Display the total amount rung up by each staff member in August of 2005.
 SELECT CONCAT(staff.first_name, ' ', staff.last_name) as employee, sum(payment.amount) AS amount FROM payment
-INNER JOIN staff ON payment.staff_id=staff.staff_id 
+JOIN staff ON payment.staff_id=staff.staff_id
+WHERE LEFT(payment.payment_date, 7) = '2005-08' 
 GROUP BY employee;
    
    -- Which actor has appeared in the most films?
 SELECT CONCAT(actor.first_name, ' ', actor.last_name) AS actorname, COUNT(film_actor.film_id) AS films FROM actor
-INNER JOIN film_actor ON actor.actor_id=film_actor.actor_id
+JOIN film_actor ON actor.actor_id=film_actor.actor_id
 GROUP BY actorname
 LIMIT 1;     
    
    -- Most active customer (the customer that has rented the most number of films)
 SELECT CONCAT(customer.first_name, ' ', customer.last_name) as customername, COUNT(rental.rental_id) AS rentals FROM customer
-INNER JOIN rental ON customer.customer_id=rental.customer_id
+JOIN rental ON customer.customer_id=rental.customer_id
 GROUP BY customername
 LIMIT 1;  
    
    
    -- Display the first and last names, as well as the address, of each staff member.
 SELECT CONCAT(staff.first_name, ' ', staff.last_name) AS name, address.address as staff_address FROM staff
-INNER JOIN address ON staff.address_id=address.address_id;  
+JOIN address ON staff.address_id=address.address_id;  
 
    
    -- List each film and the number of actors who are listed for that film.
 SELECT film.title, COUNT(film_actor.actor_id) AS actors FROM film
-INNER JOIN film_actor ON film.film_id=film_actor.film_id
+JOIN film_actor ON film.film_id=film_actor.film_id
 GROUP BY film.title;
+   
    
    -- Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name.
 SELECT CONCAT(customer.last_name, ', ', customer.first_name) as name, SUM(payment.amount) FROM payment
-INNER JOIN customer ON customer.customer_id=payment.customer_id
+JOIN customer ON customer.customer_id=payment.customer_id
 GROUP BY name 
 ORDER BY name ASC;  
    
+   
    -- List number of films per category.
 SELECT category.name, COUNT(film_category.film_id) FROM category
-INNER JOIN film_category ON category.category_id=film_category.category_id
+JOIN film_category ON category.category_id=film_category.category_id
 GROUP BY category.name;
---
+++ sqllab5.sql
@@ -0,0 +1,31 @@
+USE sakila;
+
+
+--    Write a query to display for each store its store ID, city, and country.
+
+
+
+--    Write a query to display how much business, in dollars, each store brought in.
+
+
+--    Which film categories are longest?
+
+
+--    Display the most frequently rented movies in descending order.
+
+
+--    List the top five genres in gross revenue in descending order.
+
+
+--    Is "Academy Dinosaur" available for rent from Store 1?
+
+
+--    Get all pairs of actors that worked together.
+
+
+--    Get all pairs of customers that have rented the same film more than 3 times.
+
+
+--    For each film, list actor that has acted in more films.
+
+
